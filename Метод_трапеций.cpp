ee956476d6140718e69b49265e6c0fca836a4a58
ArrowOfDevil <47675041+ArrowOfDevil@users.noreply.github.com>
2024-03-28 02:22:10 +0300

    Update Метод_трапеций.cpp

--- Метод_трапеций.cpp
+++ Метод_трапеций.cpp
@@ -1,1 +1,45 @@
+#include <locale.h>
+#include <math.h>
 
+#include <cmath>
+#include <iostream>
+using namespace std;
+typedef double (*pointFunc)(double);
+double f(double x) { return (10 - x); }
+double avg_rectangle_integral(pointFunc f, double a, double b, int n) {
+  double x, h;
+  double sum = 0.0;
+  h = (b - a) / n;  // шаг
+
+  for (int i = 0; i <= n; i++) {
+    x = a + i * h;
+    if (i == 0 || i == n)
+      sum += 0.5 * f(x);
+    else
+      sum += f(x);
+    
+  }
+  return (sum * h);  // приближенное значение интеграла равно
+                     // сумме площадей прямоугольников
+}
+int main() {
+  setlocale(LC_ALL, "Russian");
+  double a, b, eps;
+  double s1, s;
+  int n = 1;  // начальное число шагов
+
+  cout << "Введите левую границу интегрирования a = ";
+  cin >> a;
+  cout << "\nВведите правую границу интегрирования b = ";
+  cin >> b;
+  cout << "\nВведите требуемую точность eps = ";
+  cin >> eps;
+  s1 = avg_rectangle_integral(f, a, b, n);  // первое приближение для интеграла
+  do {
+    s = s1;     // второе приближение
+    n = 2 * n;  // увеличение числа шагов в два раза,
+                // т.е. уменьшение значения шага в два раза
+    s1 = avg_rectangle_integral(f, a, b, n);
+  } while (fabs(s1 - s) > eps);  // сравнение приближений с заданной точностью
+  cout << "\nИнтеграл = " << s1 << endl;
+}
